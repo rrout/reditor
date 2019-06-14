9e5ff52531147d4fd897d4a6975f282f7451a025
Rashmi Ranjan Rout <rashmiranjan.rout@us.fujitsu.com>
2019-06-14 07:57:47 -0500

    SCRIPTS

--- scripts/reditor_setup_dev_env.sh
+++ scripts/reditor_setup_dev_env.sh
@@ -3,39 +3,69 @@ set -e
 HN=`hostname`
 DN=`/usr/bin/domainname`
 
-echo -e "\n=====> Checking VNC SERVER In path: ${HOME}"
-if type vncserver > /dev/null; then
-    echo "========= VNC SERVER LIST =========="
-    echo "VNC server `vncserver -list`"
-    cat ~/.vnc/xstartup
-    echo -e "\n=====>Starting vncserver at :5903"
-    vncserver :3
-else
-    echo "VNC server not Installed"
+PROG=`basename "$0"`
+function usage {
+    echo "Usage : $PROG -option < -vnc | -novnc | -wetty | -jupyter | -all >"
+    echo "Example"
+    echo "        : $PROG -vnc"
+    echo "        : $PROG -wetty"
+    echo "        : $PROG -jupyter"
+    echo "        : $PROG -novnc"
+    echo "        : $PROG -all (This excludes -vnc)"
+}
+
+
+if [ $# != 1 ]; then
+    usage
+    exit 0
 fi
 
-echo -e "\n=====> Checking noVNC In path: ${HOME}"
-if [ -d ~/noVNC/noVNC-1.1.0 ]; then
-    echo 'NOTE:  ~/noVNC/noVNC-1.1.0  exists'
-    cd  ~/noVNC/noVNC-1.1.0
-    ./utils/launch.sh --vnc localhost:5903 &
-    echo -e "\n=====> noVNC is Started: You can log in to: http://$HN.$DN:6080/vnc.html?host=$HN&port=6080" >&2
+if [ "$1" == "-vnc" ] || [ "$1" == "-wetty" ] || [ "$1" == "-novnc" ]
+    [ "$1" == "-jupyter" ]  || [ "$1" == "-all" ]; then
+    echo $1
 else
-    cat<<EOF
-    Install noVNC in ${HOME}
-    Steps:
+    echo "Unknown option --- $1"
+    usage
+fi
+
+if [ "$1" == "-vnc" ];then
+    echo -e "\n=====> Checking VNC SERVER In path: ${HOME}"
+    if type vncserver > /dev/null; then
+        echo "========= VNC SERVER LIST =========="
+        echo "VNC server `vncserver -list`"
+        cat ~/.vnc/xstartup
+        echo -e "\n=====>Starting vncserver at :5903"
+        vncserver :3
+    else
+        echo "VNC server not Installed"
+    fi
+fi
+
+if [ "$1" == "-novnc" ] || [ "$1" == "-all" ];then
+    echo -e "\n=====> Checking noVNC In path: ${HOME}"
+    if [ -d ~/noVNC/noVNC-1.1.0 ]; then
+        echo 'NOTE:  ~/noVNC/noVNC-1.1.0  exists'
+        cd  ~/noVNC/noVNC-1.1.0
+        ./utils/launch.sh --vnc localhost:5903 &
+        echo -e "\n=====> noVNC is Started: You can log in to: http://$HN.$DN:6080/vnc.html?host=$HN&port=6080" >&2
+    else
+cat<<EOF
+        Install noVNC in ${HOME}
+        Steps:
         Start vncserver :
-            $ vncserver :3
+        $ vncserver :3
         Get noVNC:
-            mkdir ~/noVNC
-            cd ~/noVNC
-            $ wget https://github.com/novnc/noVNC/archive/v1.1.0.zip
-            $ extract the tarfile 
-            $ cd ~/noVNC/noVNC-1.1.0/
+        mkdir ~/noVNC
+        cd ~/noVNC
+        $ wget https://github.com/novnc/noVNC/archive/v1.1.0.zip
+        $ extract the tarfile 
+        $ cd ~/noVNC/noVNC-1.1.0/
         Start noVNC
-            ./utils/launch.sh --vnc localhost:5903 &
+        ./utils/launch.sh --vnc localhost:5903 &
         Fallow instructions
 EOF
+    exit 0
+    fi
 fi
 
 echo -e "\n=====> Checking Node.js In path: ${HOME}"
@@ -48,20 +78,23 @@ else
     exit 0
 fi
 
-echo -e "\n=====> Checking Wetty In path ${HOME}"
-if  [ -x "$(command -v wetty)" ]; then
-    echo 'NOTE: wetty is installed.' >&2
-    echo "Starting Wetty"
-    wetty &
-    echo -e "\n=====> Wetty is Started: You can log in to: http://$HN.$DN:3000" >&2
-else
+if [ "$1" == "-wetty" ] || [ "$1" == "-all" ];then
+    echo -e "\n=====> Checking Wetty In path ${HOME}"
+    if  [ -x "$(command -v wetty)" ]; then
+        echo 'NOTE: wetty is installed.' >&2
+        echo "Starting Wetty"
+        wetty &
+        echo -e "\n=====> Wetty is Started: You can log in to: http://$HN.$DN:3000" >&2
+    else
 cat<<EOF
-    install Wetty
-        Read Got hub:  git clone https://github.com/krishnasrinivas/wetty.git
-        $ npm i -g wetty.js
-    Start Wettty
-        $ wetty
+        install Wetty
+            Read Got hub:  git clone https://github.com/krishnasrinivas/wetty.git
+            $ npm i -g wetty.js
+        Start Wettty
+            $ wetty
 EOF
+    exit 0
+    fi
 fi
 
 echo -e "\n=====> Checking ~/.local directory In path: ${HOME}"
@@ -69,6 +102,8 @@ if [ -d ~/.local ]; then
     echo 'Error: ~/.local exists'
     echo "=====> Exporting ~/.local/bin to Global Env Path: $PATH"
     export PATH=~/.local/bin:$PATH
+else
+    exit 0
 fi
 
 echo -e "\n=====> Checking python3 directory In path"
@@ -84,6 +119,7 @@ Install Python 3.7
 Export The path
     export PATH=~/.local/bin:$PATH
 EOF
+    exit 0
 fi
 
 
@@ -102,25 +138,28 @@ Install pip3 OR Upgrade pip3
 Check pip3 version
     pip3 --version
 EOF
+    exit 0
 fi
 
-echo -e "\n=====> Checking Jupyter In path ${HOME}"
-if  [ -x "$(command -v jupyter)" ]; then
-    echo 'jupyter is installed.' >&2
-    cd /data/users/rrout/
-    echo 'Starting Jupyter......' >&2
-    jupyter lab --ip=0.0.0.0 --no-browser &
-    echo -e "\n=====> Jupyter is Started: You can log in to: http://$HN.$DN:3001" >&2
-else
+if [ "$1" == "-jupyter" ] || [ "$1" == "-all" ];then
+    echo -e "\n=====> Checking Jupyter In path ${HOME}"
+    if  [ -x "$(command -v jupyter)" ]; then
+        echo 'jupyter is installed.' >&2
+        cd /data/users/rrout/
+        echo 'Starting Jupyter......' >&2
+        jupyter lab --ip=0.0.0.0 --no-browser &
+        echo -e "\n=====> Jupyter is Started: You can log in to: http://$HN.$DN:3001" >&2
+    else
 cat<<EOF
-Install jupyterlab
-    export PATH=~/.local/bin:$PATH
-    pip3 install --user jupyterlab
-    pip3 list
+    Install jupyterlab
+        export PATH=~/.local/bin:$PATH
+        pip3 install --user jupyterlab
+        pip3 list
 
-Start jupyter lab
-    jupyter lab --ip=0.0.0.0 --no-browser
+    Start jupyter lab
+        jupyter lab --ip=0.0.0.0 --no-browser
 EOF
+    fi
 fi
 
 
