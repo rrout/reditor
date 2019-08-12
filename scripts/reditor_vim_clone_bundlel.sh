7e71388af63b96e6f067720afc97265383d12293
Rashmi Ranjan Rout <45759653+rrout@users.noreply.github.com>
2019-08-12 19:05:14 +0530

    Create reditor_vim_clone_bundlel.sh

---
+++ scripts/reditor_vim_clone_bundlel.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+set -e
+cd ~/reditor/vim/.vim/bundle
+git clone https://github.com/mileszs/ack.vim.git
+git clone  https://github.com/ronakg/quickr-cscope.vim.git
+git clone https://github.com/jaxbot/semantic-highlight.vim.git
+git clone git://github.com/jpalardy/spacehi.vim.git
+git clone https://github.com/majutsushi/tagbar.git
+git clone git://github.com/ntpeters/vim-better-whitespace.git
+git clone https://github.com/mhinz/vim-signify.git
+git clone https://github.com/tpope/vim-fugitive.git
+git clone https://github.com/vim-airline/vim-airline.git
+git clone git://github.com/airblade/vim-gitgutter.git
+
