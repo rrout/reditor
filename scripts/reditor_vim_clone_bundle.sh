5e0fc1921ce307d2e5dd2368178563a69df4f24d
Rashmi Ranjan Rout <rashmiranjan.rout@us.fujitsu.com>
2019-08-22 10:20:58 -0500

    reditor_vim_clone_bundle.sh : removed spaceHi and Quicker-cscope

--- scripts/reditor_vim_clone_bundle.sh
+++ scripts/reditor_vim_clone_bundle.sh
@@ -2,9 +2,9 @@
 set -e
 cd ~/reditor/vim/.vim/bundle
 git clone https://github.com/mileszs/ack.vim.git
-git clone  https://github.com/ronakg/quickr-cscope.vim.git
+#git clone  https://github.com/ronakg/quickr-cscope.vim.git
 git clone https://github.com/jaxbot/semantic-highlight.vim.git
-git clone git://github.com/jpalardy/spacehi.vim.git
+#git clone git://github.com/jpalardy/spacehi.vim.git
 git clone https://github.com/majutsushi/tagbar.git
 git clone git://github.com/ntpeters/vim-better-whitespace.git
 git clone https://github.com/mhinz/vim-signify.git
