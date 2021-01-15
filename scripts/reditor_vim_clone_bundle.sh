28acd5fe37380b666851a4c0d9290c22480ddacf
Rashmi Ranjan Rout <rashmi.rout@fujitsu.com>
2021-01-15 07:55:23 -0600

    AutoComplPop Settings

--- scripts/reditor_vim_clone_bundle.sh
+++ scripts/reditor_vim_clone_bundle.sh
@@ -11,5 +11,6 @@ git clone https://github.com/mhinz/vim-signify.git
 git clone https://github.com/tpope/vim-fugitive.git
 git clone https://github.com/vim-airline/vim-airline.git
 #git clone https://github.com/justmao945/vim-clang.git
+git clone https://github.com/vim-scripts/AutoComplPop.git
 git clone git://github.com/airblade/vim-gitgutter.git
 
